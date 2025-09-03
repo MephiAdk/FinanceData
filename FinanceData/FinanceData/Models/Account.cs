d5e565fe4ebcaacf86a582aa41df5b7ea7284018
Adrien DECONINCK <adrien.deconinck+github@protonmail.com>
2025-09-03 18:56:37 +0200

    add models account and balanceEntry

---
+++ FinanceData/FinanceData/Models/Account.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace FinanceData.Models
+{
+    public class Account
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ FinanceData/FinanceData/Models/BalanceEntry.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+
+namespace FinanceData.Models
+{
+    public class BalanceEntry
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public decimal Value { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [Indexed]
+        public int AccountId { get; set; }
+    }
+}
